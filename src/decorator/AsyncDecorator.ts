3269f8b34f872c8b2cf0373a48e24bd3ecce4d33
tkt <tktcorporation.go@gmail.com>
2020-07-26 13:06:31 +0900

    add implementation of async decorators

---
+++ src/decorator/AsyncDecorator.ts
@@ -0,0 +1,42 @@
+export type VoidFunc = (() => Promise<void>) | (() => void);
+export type ErrorArgVoidFunc = ((error: any) => Promise<void>) | (() => void);
+/**
+ * A base of Decorator for using async/await
+ * @param beforeFunc no args void func
+ * @param afterFunc no args void func
+ * @param catchFunc error arg void func
+ * @param finalFunc no args void func
+ */
+export function AsyncDecorator(
+  beforeFunc?: VoidFunc,
+  afterFunc?: VoidFunc,
+  catchFunc?: ErrorArgVoidFunc,
+  finalFunc?: VoidFunc,
+): MethodDecorator {
+  return function (
+    target: Object,
+    methodName: string | symbol,
+    descriptor: PropertyDescriptor,
+  ) {
+    const originalMethod = descriptor.value;
+    descriptor.value = function (...args: any[]) {
+      const originalMethodFunc = async () => {
+        return originalMethod.apply(this, [...args]);
+      };
+      const asyncFunc = async () => {
+        try {
+          await beforeFunc?.();
+          const result = await originalMethodFunc();
+          await afterFunc?.();
+          return result;
+        } catch (e) {
+          if (catchFunc === undefined) throw e;
+          await catchFunc?.(e);
+        } finally {
+          await finalFunc?.();
+        }
+      };
+      return asyncFunc();
+    };
+  };
+}
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { AsyncDecorator } from "./decorator/AsyncDecorator";
