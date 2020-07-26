819e83b85b365b5a1c43691dd6243116a5a10b28
tkt <tktcorporation.go@gmail.com>
2020-07-26 17:27:07 +0900

    update test code

--- src/decorator/AsyncMethodDecorator.spec.ts
+++ src/decorator/AsyncMethodDecorator.spec.ts
@@ -1,38 +1,96 @@
 import { AsyncMethodDecorator } from './AsyncMethodDecorator';
 
-const delayLog = async (str: string, ms: number) => {
+const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+const delayPush = async (arr: string[], str: string, ms: number) => {
   await delay(ms);
-  // console.log(str);
+  arr.push(str);
 };
-const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+const delayPushAndThrow = async (
+  error: any,
+  arr: string[],
+  str: string,
+  ms: number,
+) => {
+  await delay(ms);
+  arr.push(str);
+  throw error;
+};
+
+enum Timing {
+  before = 'before',
+  after = 'after',
+  catched = 'catched',
+  finaly = 'finaly',
+}
 
 // This is not so useful.
 describe('AsyncMethodDecorator', () => {
-  describe('AsyncMethodDecorator', () => {});
-  class User {
-    @AsyncMethodDecorator(
-      async () => delayLog('まえ', 4000),
-      async () => delayLog('あと', 2000),
-      async () => delayLog('しっぱい', 1000),
-    )
-    async hello() {
-      // console.log('こんにちは');
+  it('validate timing of before, after, finaly', async () => {
+    class Sample {
+      static resultArray: string[] = [];
+      @AsyncMethodDecorator(
+        async () => delayPush(Sample.resultArray, Timing.before, 4000),
+        async () => delayPush(Sample.resultArray, Timing.after, 2000),
+        async () => delayPush(Sample.resultArray, Timing.catched, 1000),
+        async () => delayPush(Sample.resultArray, Timing.finaly, 1000),
+      )
+      async hello() {
+        await delayPush(Sample.resultArray, 'hello', 5000);
+      }
     }
-
-    @AsyncMethodDecorator(
-      async () => delayLog('まえ', 4000),
-      async () => delayLog('あと', 2000),
-      async () => delayLog('しっぱい', 1000),
-    )
-    async nothello() {
-      // console.log('こんにちは');
-      throw 'えらー';
+    await new Sample().hello();
+    expect(Sample.resultArray).toStrictEqual([
+      Timing.before,
+      'hello',
+      Timing.after,
+      Timing.finaly,
+    ]);
+  }, 20000);
+  it('validate timing of before, catched, finaly', async () => {
+    class Sample {
+      static resultArray: string[] = [];
+      @AsyncMethodDecorator(
+        async () => delayPush(Sample.resultArray, Timing.before, 4000),
+        async () => delayPush(Sample.resultArray, Timing.after, 2000),
+        async () => delayPush(Sample.resultArray, Timing.catched, 1000),
+        async () => delayPush(Sample.resultArray, Timing.finaly, 1000),
+      )
+      async fail() {
+        await delayPush(Sample.resultArray, 'fail', 5000);
+        throw new Error();
+      }
     }
-  }
-  it('せいこう', async () => {
-    await new User().hello();
-  }, 10000);
-  it('しっぱい', async () => {
-    await new User().nothello();
-  }, 10000);
+    await new Sample().fail();
+    expect(Sample.resultArray).toStrictEqual([
+      Timing.before,
+      'fail',
+      Timing.catched,
+      Timing.finaly,
+    ]);
+  }, 20000);
+  it('validate timing of before, catched, finaly, throw', async () => {
+    class Sample {
+      static resultArray: string[] = [];
+      @AsyncMethodDecorator(
+        async () => delayPush(Sample.resultArray, Timing.before, 4000),
+        async () => delayPush(Sample.resultArray, Timing.after, 2000),
+        async e =>
+          delayPushAndThrow(e, Sample.resultArray, Timing.catched, 1000),
+        async () => delayPush(Sample.resultArray, Timing.finaly, 1000),
+      )
+      async fail() {
+        await delayPush(Sample.resultArray, 'fail', 5000);
+        throw new Error();
+      }
+    }
+    await expect(async () => await new Sample().fail()).rejects.toThrowError(
+      Error,
+    );
+    expect(Sample.resultArray).toStrictEqual([
+      Timing.before,
+      'fail',
+      Timing.catched,
+      Timing.finaly,
+    ]);
+  }, 20000);
 });
